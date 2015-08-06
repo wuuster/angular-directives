d8a0b3d2cfd0f3c317eaff7ba3c82a80b717db7b
Koiwu Beyan <wuuster@gmail.com>
2015-08-06 10:49:15 -0700

    Share data between controllers with services

---
+++ angular-services-share-data-between-controllers/share-data-services.js
@@ -0,0 +1,20 @@
+angular.module('app', [])
+    .controller('ctrl', function ($scope, appData) {
+        $scope.getInfo = appData.getData();
+        $scope.addInfo = 'foo';
+        appData.addData($scope.addInfo);
+    })
+    .service('appData', function () {
+    data = [];
+    var addData = function (i) {
+        var args = data.push(i);
+    }
+    var getData = function () {
+        return data;
+    }
+
+    return {
+        getData: getData,
+        addData: addData
+    }
+});
